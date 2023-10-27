8e91210dab6f65ae055e58a4ea592b5331223086
Ramon Yanez OShee <ramon.yanez@gmail.com>
2023-10-27 19:36:46 +1300

    adds logging debug

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { LoggerService } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error','warn','debug'],
+  });
   await app.listen(3000);
 }
 bootstrap();
